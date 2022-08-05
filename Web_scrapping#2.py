db700d02b7d2916e45aafe80591d373631067c92
Breno Rossi <brenorossi18@gmail.com>
2022-08-04 21:55:48 -0300

    AutomacaoFuncionalV2

---
+++ Web_scrapping#2.py
@@ -0,0 +1,54 @@
+# Importando bibliotecas
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import pandas as pd
+
+# Criando lista
+cards = []
+
+# Obtendo o HTML e o total de páginas
+response = urlopen('https://alura-site-scraping.herokuapp.com/index.php')
+html = response.read().decode('utf-8')
+soup = BeautifulSoup(html, 'html.parser')
+pages = int(soup.find('span', class_="info-pages").get_text().split()[-1])
+
+# Iterando nas páginas do site
+for i in range(pages):
+
+    # Obtendo o HTML
+    response = urlopen(
+        'https://alura-site-scraping.herokuapp.com/index.php?page=' + str(i + 1))
+    html = response.read().decode('utf-8')
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # Obtendo as TAGs de interesse
+    anuncios = soup.find(
+        'div', {"id": "container-cards"}).findAll('div', class_="card")
+
+    # Coletando as informações dos CARDS
+    for anuncio in anuncios:
+        card = {}
+
+        # Valor
+        card['value'] = anuncio.find('p', {'class': 'txt-value'}).getText()
+
+        # Informações
+        infos = anuncio.find('div', {'class': 'body-card'}).findAll('p')
+        for info in infos:
+            card[info.get('class')[0].split('-')[-1]] = info.get_text()
+
+        # Acessórios
+        items = anuncio.find('div', {'class': 'body-card'}).ul.findAll('li')
+        items.pop()
+        acessorios = []
+        for item in items:
+            acessorios.append(item.get_text().replace('► ', ''))
+        card['items'] = acessorios
+
+        # Adicionando resultado a lista cards
+        cards.append(card)
+
+
+# Criando um DataFrame com os resultados
+dataset = pd.DataFrame(cards)
+dataset.to_csv('dataset.csv', sep=';', index=False, encoding='utf-8-sig')
